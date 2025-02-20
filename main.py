cb0726f0a5ddcb1270bf91b16a5445b79f59572d
CidKagenou3009 <cidkagenou0195@gmail.com>
2025-02-20 16:07:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bot import Bot
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1001991235299
+pyrogram.utils.MIN_CHANNEL_ID = -1002203178713
 
 if __name__ == "__main__":
     Bot().run()
